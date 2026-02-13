eef3cf7cb6d27ba3da11c18cf98c8ae23effe5d2
junior2244 <158349273+junior2244@users.noreply.github.com>
2026-02-13 13:23:33 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
 const supabase = createClient(
-  "YOUR_SUPABASE_URL",
-  "YOUR_PUBLISHABLE_KEY"
+  "https://iwxrzenytjoxdfemfbev.supabase.co",
+  "sb_publishable_HuXkfqB0ZlMnIpB3014hKw_pRuUPSYF"
 );
 
 async function login() {
